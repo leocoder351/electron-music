2650cee170b799ff2ed944eb236d9d3d81ed9e13
liuxuan <liuxuan@unicloud.com>
2019-09-29 20:11:30 +0800

    add AppBrowser

--- main.js
+++ main.js
@@ -1,23 +1,32 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
 
-app.on('ready', () => {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
-  mainWindow.loadFile('./renderer/index.html')
-  ipcMain.on('add-music-window', (event, content) => {
-    const addWindow = new BrowserWindow({
-      width: 500,
-      height: 400,
+class AppWindow extends BrowserWindow {
+  constructor(config, fileLocation) {
+    const basicConfig = {
+      width: 800,
+      height: 600,
+      show: false,
       webPreferences: {
         nodeIntegration: true
-      },
-      parent: mainWindow
+      }
+    }
+    // const finalConfig = Object.assign(basicConfig, config)
+    const finalConfig = { ...basicConfig, ...config}
+    super(finalConfig)
+    this.loadFile(fileLocation)
+    this.once('ready-to-show', () => {
+      this.show()
     })
-    addWindow.loadFile('./renderer/add.html')
+  }
+}
+
+app.on('ready', () => {
+  const mainWindow = new AppWindow({}, './renderer/index.html')
+  ipcMain.on('add-music-window', (event, content) => {
+    const addWindow = new AppWindow({
+      width: 500,
+      height: 400,
+      parent: mainWindow
+    }, './renderer/add.html')
   })
 })
